2c16c6ca2c3bcc8b2f460641b4ec389bb1072fb1
pants007 <qrw992@alumni.ku.dk>
2022-02-19 22:41:49 +0100

    log the card query
    Signed-off-by: pants007 <qrw992@alumni.ku.dk>

--- index.js
+++ index.js
@@ -9,34 +9,6 @@ async function main(){
   const repoName = github.context.payload.repository.name;
   const ownerName = github.context.payload.repository.owner.login;
   const octokit = github.getOctokit(myToken);
-  var cardQuery = await octokit.graphql(`
-  {
-    repository(owner:"${ownerName}", name:"${repoName}"){
-      projects(first: 5){
-        nodes{
-          name
-          columns(first: 3){
-            nodes{
-              name
-              id
-              cards{
-                nodes{
-                  id
-                  content{
-                    ... on Issue{
-                      title
-                      number
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  }`);
-
   var projects = await octokit.rest.projects.listForRepo({
     owner: ownerName,
     repo: repoName
@@ -56,6 +28,7 @@ async function main(){
   const projectCards = await octokit.rest.projects.listCards({
     column_id:fromColumn.id
   });
+  //hacky, ugly way of getting the issue number from the card. extracts the number from the content_url field in the response
   const projectCard = projectCards.data.find(card => card.content_url.split('').reverse().join('').substring(0, card.content_url.split('').reverse().join('').search('/')).split('').reverse().join('') == issueNumber);
   var movedCard = await octokit.rest.projects.moveCard({
     card_id: projectCard.id,
@@ -63,8 +36,7 @@ async function main(){
     column_id: toColumn.id
   });
   core.setOutput('card-id', `${projectCard.id}`);
-  return `Added the labels \'${labelsToAdd}\' to issue #${issueNumber}\n
-  The card was moved to column \'${toColumn}\' in ${repoName}/projects/${projectBoard}`;
+  return `The card was moved to column \'${toColumn}\' in ${repoName}/projects/${projectBoard}`;
 }
 
 main().then(
