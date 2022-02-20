850a42c402892e972de94e37d0fc14ed591060c5
pants007 <qrw992@alumni.ku.dk>
2022-02-20 13:13:37 +0100

    log the card query
    Signed-off-by: pants007 <qrw992@alumni.ku.dk>

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const { createReadStream } = require('fs');
 
 async function main(){
   const myToken = core.getInput('repo-token');
@@ -29,12 +30,87 @@ async function main(){
     column_id:fromColumn.id
   });
   //hacky, ugly way of getting the issue number from the card. extracts the number from the content_url field in the response
-  const projectCard = projectCards.data.find(card => card.content_url.split('').reverse().join('').substring(0, card.content_url.split('').reverse().join('').search('/')).split('').reverse().join('') == issueNumber);
+  const projectCard = projectCards.data.find(card => card.content_url.substring(card.content_url.lastIndexOf('/')+1, card.content_url.length) == issueNumber);
   var movedCard = await octokit.rest.projects.moveCard({
     card_id: projectCard.id,
     position: 'top',
     column_id: toColumn.id
   });
+  core.setOutput('card-id', `${projectCard.id}`);
+  return `The card was moved to column \'${toColumn}\' in ${repoName}/projects/${projectBoard}`;
+}
+
+async function main(){
+  const myToken = core.getInput('repo-token');
+  const issueNumber = core.getInput('issue-number');
+  const fromColumnName = core.getInput('from-column');
+  const toColumnName = core.getInput('to-column');
+  const repoName = github.context.payload.repository.name;
+  const ownerName = github.context.payload.repository.owner.login;
+  const octokit = github.getOctokit(myToken);
+
+  //hit up graphql for the projects attached to repo
+  const projectQuery = `
+  {
+      repository(owner:"${ownerName}", name:"${repoName}"){
+        projects(first: 5){
+          nodes{
+            name
+            columns(first: 5){
+              nodes{
+                name
+                id
+                cards{
+                  nodes{
+                    id
+                    databaseId
+                    content{
+                      ... on Issue{
+                        number
+    }}}}}}}}}
+  }`;
+  var projectsResponse = await octokit.graphql(projectQuery);
+
+  //find the project board the issue is attached to
+  let project = response.data.repository.projects.nodes.find(proj => {
+    let correctCol = proj.columns.nodes.find(col => {
+        let contentCards = col.cards.nodes.filter(card => card.state == 'CONTENT_ONLY');
+        let isCardThere = contentCards.find(card => card.content.number == issueNumber);
+        return isCardThere;
+      });
+    return correctCol;
+  });
+  //filter out non-content cards
+  project.columns.nodes = project.columns.nodes.map(col => {
+    let cards = col.cards.nodes.filter(card => card.state == 'CONTENT_ONLY');
+    col.cards.nodes = cards;
+    return col;
+  });
+  //find the column the card is in, and verify that it has the specified name
+  let fromColumn = project.columns.nodes.find(col => {
+    let isCardThere = col.cards.nodes.find(card => card.content.number == issueNumber);
+    return isCardThere && col.name == fromColumnName;
+  });
+  let toColumn = project.columns.nodes.find(col => col.name == toColumnName);
+  if (!fromColumn){
+    throw 'The chosen fromColumn does not exist, ending early'
+  }else if(!toColumn){
+    throw 'The chosen toColumn does not exist, ending early'
+  }
+
+  let card = fromColumn.cards.nodes.find(card => card.content.number == issueNumber);
+
+  const moveQuery = `
+  {
+    moveProjectCard(
+      input:{
+        cardId: "${card.id}",
+        columnId:"${toColumn.id}"}){
+      clientMutationId
+    }
+  }
+  `
+  var movedCard = await octokit.graphql(moveQuery);
   core.setOutput('card-id', `${projectCard.id}`);
   return `The card was moved to column \'${toColumn}\' in ${repoName}/projects/${projectBoard}`;
 }
