8148ef24c9bb572c45e9aee76f3cacf3108d7044
pants007 <qrw992@alumni.ku.dk>
2022-02-20 13:13:53 +0100

    log the card query
    Signed-off-by: pants007 <qrw992@alumni.ku.dk>

--- index.js
+++ index.js
@@ -1,44 +1,5 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const { createReadStream } = require('fs');
-
-async function main(){
-  const myToken = core.getInput('repo-token');
-  const issueNumber = core.getInput('issue-number');
-  const fromColumnName = core.getInput('from-column');
-  const toColumnName = core.getInput('to-column');
-  const repoName = github.context.payload.repository.name;
-  const ownerName = github.context.payload.repository.owner.login;
-  const octokit = github.getOctokit(myToken);
-  var projects = await octokit.rest.projects.listForRepo({
-    owner: ownerName,
-    repo: repoName
-  });
-
-  const projectName = 'code';
-  const project = projects.data.find(project => project.name === projectName);
-
-  if (project === undefined){
-    core.setOutput('project-name', 'None');
-    throw `Project \'${projectName}\' does not exist in ${ownerName}/${repoName}.`
-  }
-
-  var projectColumns = await octokit.rest.projects.listColumns({project_id:project.id});
-  const fromColumn = projectColumns.data.find(column => column.name === fromColumnName);
-  const toColumn = projectColumns.data.find(column => column.name === toColumnName);
-  const projectCards = await octokit.rest.projects.listCards({
-    column_id:fromColumn.id
-  });
-  //hacky, ugly way of getting the issue number from the card. extracts the number from the content_url field in the response
-  const projectCard = projectCards.data.find(card => card.content_url.substring(card.content_url.lastIndexOf('/')+1, card.content_url.length) == issueNumber);
-  var movedCard = await octokit.rest.projects.moveCard({
-    card_id: projectCard.id,
-    position: 'top',
-    column_id: toColumn.id
-  });
-  core.setOutput('card-id', `${projectCard.id}`);
-  return `The card was moved to column \'${toColumn}\' in ${repoName}/projects/${projectBoard}`;
-}
 
 async function main(){
   const myToken = core.getInput('repo-token');
